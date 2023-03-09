ecb9767f8ae70d21da96913f9d011a26a039df4a
{wenjunvue} <lwj@example.com>
2023-03-09 17:37:08 +0800

    1

---
+++ src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <Snow :zIndex="'2'" color="#ffffff" :swing='2' :speed='"m"' />
+  </div>
+</template>
+
+<script>
+  // @ is an alias to /src
+  import HelloWorld from '@/components/HelloWorld.vue'
+  import Snow from 'vue-niege';
+  export default {
+    name: 'HomeView',
+    components: {
+      HelloWorld,
+      Snow,
+    },
+    methods: {
+      
+    },
+    mounted() {
+      
+    }
+  }
+</script>
+<style lang="scss" scoped>
+  .home {
+    height: 100vh;
+    background: #000;
+  }
+</style>
